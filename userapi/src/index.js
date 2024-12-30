2bec55d9d910e32c39a4cd9d519c329527432c3b
Quentin-Carminot <quentincarminot@icloud.com>
2024-12-30 16:49:40 +0100

    fix: part7

--- userapi/src/index.js
+++ userapi/src/index.js
@@ -13,7 +13,7 @@ let redisClient = null;
 (async () => {
   try {
     redisClient = createClient({
-      url: `redis://${process.env.REDIS_HOST || '127.0.0.1'}:${process.env.REDIS_PORT || 6379}`,
+      url: `redis://${process.env.REDIS_HOST || 'redis-service.default.svc.cluster.local'}:${process.env.REDIS_PORT || 6379}`,
     });
 
     // Gestion des erreurs Redis
